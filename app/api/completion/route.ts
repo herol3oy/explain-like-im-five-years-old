ef05f97c9ff61675a2236e59c0b22ce4cc7a61c6
demah <hamedsedighi@gmail.com>
2023-07-14 17:38:58 +0200

    init basic app

---
+++ app/api/completion/route.ts
@@ -0,0 +1,42 @@
+import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
+
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  const { prompt } = await req.json();
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    temperature: 0,
+    max_tokens: 150,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    stop: [":"],
+    stream: true,
+    prompt: `
+    Below is text describing how to answer when a user asks to "explain like I'm five years old in maximum 1000 characters".
+    Your goal is to:
+    - Explain a text like I'm five years old in maximum 1000 characters
+    Here are some examples:
+    - Text: Explain Higgs boson like I'm five years old in maximum 1000 characters
+    - Answer: The Higgs boson is like a tiny, invisible ball that gives other particles their mass. Imagine a bunch of particles at a party. The Higgs boson is like a cool guest who gives a special feeling to everyone it touches, making them slower and heavier, just like when you wear a heavy backpack. Without the Higgs boson, everything would zoom around really fast and be weightless, like a bunch of super-fast superheroes.
+    - Text: Explain Big data like I'm five years old in maximum 1000 characters
+    - Answer Big data is like having a super big box of Legos with lots and lots of different colors and shapes. You have so many Legos that you can't count them all by yourself. It's like having a huge mountain of toys! But instead of Legos, it's information and numbers. People collect all this information from different places, like the internet or machines. They use special tools to organize and understand all the information. It's like building cool things with Legos, but instead, they build important things with the big data, like finding patterns, solving problems, and making smart decisions.
+    Below is the text:
+    TEXT: explain ${prompt} like I'm five years old in maximum 1000 characters 
+    YOUR RESPONSE:
+  `,
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Explain like I&aposm five years old</title>
+      <meta name="description" content="A super smart app that explain topic and terms like we are 5 years old!" />
+      <meta
+     name="keywords"
+     content="AI, learning, question, child, explain, discovery, genius"
+   />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+}
